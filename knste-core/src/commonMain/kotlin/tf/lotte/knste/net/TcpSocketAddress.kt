7ad505e50777829f543d80e8276c999eeeecc2ee
Lura Skye Revuwution <l@veriny.tf>
2020-09-12 18:29:36 +0100

    WIP socket address resolution

--- knste-core/src/commonMain/kotlin/tf/lotte/knste/net/TcpSocketAddress.kt
+++ knste-core/src/commonMain/kotlin/tf/lotte/knste/net/TcpSocketAddress.kt
@@ -9,6 +9,10 @@
 
 package tf.lotte.knste.net
 
+import tf.lotte.knste.net.dns.AddressResolver
+import tf.lotte.knste.net.dns.GlobalResolver
+import tf.lotte.knste.util.Unsafe
+
 /**
  * A socket address for TCP sockets.
  */
@@ -17,6 +21,32 @@ public class TcpSocketAddress(
     /** The port of this socket address. */
     public val port: Int
 ) : SocketAddress(ip) {
+    public companion object {
+        /**
+         * Resolves a [host] and [port] combination into a [TcpSocketAddress].
+         *
+         * @param resolver: The [AddressResolver] to resolve the address with.
+         * @param preferIpv6: If IPv6 should be preferred to create the connection. (Default true)
+         */
+        @OptIn(Unsafe::class)
+        public fun resolve(
+            host: String, port: Int, resolver: AddressResolver = GlobalResolver,
+            preferIpv6: Boolean = true
+        ): TcpSocketAddress {
+            val addrInfo = resolver.getaddrinfo(
+                host, service = port,
+                family = AddressFamily.AF_UNSPEC,
+                type = SocketKind.SOCK_STREAM,
+                protocol = IPProtocol.IPPROTO_TCP
+            )
+            return if (preferIpv6) {
+                addrInfo.find { it.family == AddressFamily.AF_INET } as TcpSocketAddress
+            } else {
+                addrInfo.firstOrNull() as? TcpSocketAddress
+            } ?: TODO("handle appropriate error")
+        }
+    }
+
     override val family: AddressFamily get() = ipAddress.family
     override val kind: SocketKind get() = SocketKind.SOCK_STREAM
     override val protocol: IPProtocol get() = IPProtocol.IPPROTO_TCP
