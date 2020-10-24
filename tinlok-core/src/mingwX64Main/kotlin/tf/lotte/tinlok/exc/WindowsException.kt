a51163adffd7a07f16dd720e6e199206e1ec82cc
Lura Skye Revuwution <l@veriny.tf>
2020-10-24 16:51:43 +0100

    Add Windows error handling in Syscall.

---
+++ tinlok-core/src/mingwX64Main/kotlin/tf/lotte/tinlok/exc/WindowsException.kt
@@ -0,0 +1,20 @@
+/*
+ * Copyright (C) 2020 Charlotte Skye.
+ *
+ * This file is part of Tinlok.
+ *
+ * Tinlok is dually released under the GNU Lesser General Public License,
+ * Version 3 or later, or the Mozilla Public License 2.0.
+ */
+
+package tf.lotte.tinlok.exc
+
+import tf.lotte.cc.exc.OSException
+
+/**
+ * Thrown when an exception happens that is not otherwise turned into a
+ */
+public class WindowsException
+public constructor(
+    public val winerror: Int, message: String, cause: Throwable? = null
+) : OSException(message, cause)
--- tinlok-core/src/mingwX64Main/kotlin/tf/lotte/tinlok/system/Syscall.kt
+++ tinlok-core/src/mingwX64Main/kotlin/tf/lotte/tinlok/system/Syscall.kt
@@ -11,10 +11,10 @@ package tf.lotte.tinlok.system
 
 import kotlinx.cinterop.*
 import platform.posix.memcpy
-import platform.windows.GetEnvironmentVariableW
-import platform.windows.GetUserNameW
-import platform.windows.HRESULT
+import platform.windows.*
 import tf.lotte.cc.Unsafe
+import tf.lotte.cc.exc.*
+import tf.lotte.tinlok.exc.WindowsException
 import tf.lotte.tinlok.util.utf16ToString
 
 public actual object Syscall {
@@ -38,9 +38,88 @@ public actual object Syscall {
         }
     }
 
+    /**
+     * Formats the message for an error code.
+     */
+    @OptIn(ExperimentalUnsignedTypes::class)
     @Unsafe
-    public fun FormatMessageW(code: Int): String {
-        TODO()
+    public fun FormatMessage(code: Int): String {
+        // NB: we manually free because windows allocates it and I don't think K/N will
+        // deallocate it properly. I might be wrong about that, but this code is correct no
+        // matter what.
+        val flags = FORMAT_MESSAGE_ALLOCATE_BUFFER
+            .or(FORMAT_MESSAGE_FROM_SYSTEM)
+            .or(FORMAT_MESSAGE_IGNORE_INSERTS).toUInt()
+
+        // scary!
+        val ptr = nativeHeap.alloc<LPWSTRVar>()
+
+        val result = try {
+            FormatMessageW(
+                flags, null, code.toUInt(),
+                0,  // default language according to MSDN
+                ptr.ptr.reinterpret(), 0, null
+            )
+        } catch (e: Throwable) {
+            // just in case toUInt/reinterpret throws... (maybe in the future?)
+            HeapFree(GetProcessHeap(), 0, ptr.ptr)
+            throw e
+        }
+
+        if (result == 0U) {
+            HeapFree(GetProcessHeap(), 0, ptr.ptr)
+            throw Error("FormatMessageW failed!")
+        } else {
+            val data = ptr.value?.toKStringFromUtf16() ?: "Unknown error"
+            HeapFree(GetProcessHeap(), 0, ptr.ptr)
+            return data
+        }
+    }
+
+    /**
+     * Throws the appropriate error based on the errno.
+     */
+    @OptIn(ExperimentalUnsignedTypes::class)
+    @Unsafe
+    public fun throwErrno(): Nothing {
+        throwErrno(GetLastError().toInt())
+    }
+
+    /**
+     * Throws an error based on the error code provided.
+     */
+    @Unsafe
+    public fun throwErrno(code: Int): Nothing {
+        throw when (code) {
+            ERROR_ACCESS_DENIED -> AccessDeniedException()
+            ERROR_BROKEN_PIPE -> BrokenPipeException()
+
+            // winsock
+            WSAETIMEDOUT -> TimeoutException()
+            WSAECONNABORTED -> ConnectionAbortedException()
+            WSAECONNREFUSED -> ConnectionRefusedException()
+            WSAECONNRESET -> ConnectionResetException()
+            WSAESHUTDOWN -> SocketShutdownException()
+            WSAENETUNREACH -> NetworkUnreachableException()
+
+            else -> WindowsException(winerror = code, message = FormatMessage(code))
+        }
+    }
+
+    /**
+     * Throws an error based on the error code provided. Special function for path-related
+     * exception.
+     */
+    @Unsafe
+    public fun throwErrnoPath(code: Int, path: String): Nothing {
+        throw when (code) {
+            ERROR_FILE_NOT_FOUND, ERROR_PATH_NOT_FOUND -> FileNotFoundException(path)
+            ERROR_FILE_EXISTS -> FileAlreadyExistsException(path)
+
+            ERROR_INVALID_NAME -> IllegalArgumentException("Invalid name: $path")
+
+            else -> throwErrno(code)
+        }
     }
 
     /**
@@ -59,7 +138,7 @@ public actual object Syscall {
         }
 
         if (!SUCCEEDED(result)) {
-
+            throwErrno()
         }
 
         val count = sizePtr.value.toInt()
@@ -85,6 +164,14 @@ public actual object Syscall {
                 res.toInt()
             }
         }
+
+        // error condition?
+        if (count < buf.size) {
+            val err = GetLastError().toInt()
+            if (err == ERROR_ENVVAR_NOT_FOUND) return null
+            else throwErrno(err)
+        }
+
         return buf.utf16ToString(count)
     }
 }
