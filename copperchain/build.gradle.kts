4f65ed30d964514740e5b4425ea0d82bfe630236
Lura Skye Revuwution <l@veriny.tf>
2020-11-04 23:39:22 +0000

    Add missing publishing info.

--- copperchain/build.gradle.kts
+++ copperchain/build.gradle.kts
@@ -83,3 +83,45 @@ kotlin {
         }
     }
 }
+
+publishing {
+    publications.withType<MavenPublication> {
+        pom {
+            name.set(project.name)
+            description.set("A common module for various interfaces and utilities.")
+            url.set("https://tinlok.lotte.tf")
+
+            licenses {
+                license {
+                    name.set("LGPL-3.0-or-later")
+                    url.set("https://www.gnu.org/licenses/lgpl-3.0.en.html")
+                }
+                license {
+                    name.set("MPL-2.0")
+                    url.set("https://www.mozilla.org/en-US/MPL/2.0/")
+                }
+            }
+
+            developers {
+                developer {
+                    id.set("Constellarise")
+                    name.set("Charlotte Skye")
+                    url.set("https://lotte.tf")
+                }
+            }
+        }
+    }
+
+    repositories {
+        maven {
+            val ROOT = "https://api.bintray.com/maven/constellarise/tinlok"
+            url = uri("$ROOT/tinlok-core/;publish=1;override=1")
+
+            credentials {
+                username = System.getenv("BINTRAY_USERNAME")
+                password = System.getenv("BINTRAY_KEY")
+            }
+        }
+    }
+}
+
